a30c3bd3e9403c3b130f8bf024beb3c667aeb780
Kevin Bongart <kevin@challengepost.com>
2011-10-06 14:43:56 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AgileBoard::Application.config.secret_token = '3518f93e6b6e70368708f057ea44e0f534eba5311616b5319536625c756e289c567080d17adc0f17e1bc0c8e1efcf8cbd0e78da52b971b230f648976013bf1da'
