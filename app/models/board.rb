966cd3583921a9f9bf11d64512fb03ab75cc19b3
Kevin Bongart <kevin@challengepost.com>
2011-10-11 12:11:53 -0400

    Project creation debug.

--- app/models/board.rb
+++ app/models/board.rb
@@ -7,13 +7,18 @@ class Board < ActiveRecord::Base
   def import_projects
     unless token.nil?
       PivotalTracker::Client.token = token
+      p "Importing projects..."
       PivotalTracker::Project.all.each do |project|
+        p "  Importing project #{project.id}"
         existing_project = Project.where(:pivotal_id => project.id, :board_id => user.board).first
         if existing_project
+          p "    existing project found"
           existing_project.update_attributes(:board_id => self)
           existing_project.import_stories
         else
-          Project.create(:name => project.name, :pivotal_id => project.id, :board_id => self)
+          p "    no project found"
+          new_project = Project.create(:name => project.name, :pivotal_id => project.id, :board_id => self)
+          p "    new project created: #{new_project.id} #{new_project.name}"
         end
       end
     end
